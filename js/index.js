2c8cf420e8111df035bbc54ddc0058e6241e4542
Farrux <Farrux@gmail.com>
2025-02-28 09:00:21 +0500

    Refactor CSS for improved responsiveness; update HTML structure for hero section; clean up JavaScript code for product data display

--- js/index.js
+++ js/index.js
@@ -1,37 +1,6 @@
-// http eski variant hozirda esa https bu s sertifikat ximoya vazifasini bildiradi yani bunda ximoya kuchliroq
-// DNS server bu yozuvli manzillarni api manzilga yani (raqamlarga) o'zgartiruvchi server
-// AJAX kengtmasi bu asinhrom javascript and xml
-// api lar ikki turda bo'ladi rest api and soap api farqlari
-// Soap api malumotlar xml rest keladigan malumotlar json formatda keladi va hozirda rest api ishlatilinadi
-// Promise()
-// Promise ichida resolve va reject bular funksiyalar hisoblanadi
-// const vada = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve("Helllooo");
-//   }, 5000);
-// });
-// vada
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-// js error handling nima?
-// let a = 1;
-// console.log(a);
-
-// try {
-//     console.log(b)
-//     let b = 2;
-// } catch {
-//     console.log("Something went wrong")
-// }
-
 const url = "https://dummyjson.com/product";
 const div = document.querySelector(".hero__container");
-// const imageIcon = "././images/add-shop-bag.png";
+
 
 function showData({ products }) {
   products.forEach((product) => {
@@ -50,8 +19,7 @@ function showData({ products }) {
     // cardImage
     const image = document.createElement("img");
     image.classList.add("card__img");
-    // const cardBox = document.createElement("div");
-    // cardBox.classList.add("card__box");
+    //
     h2.textContent = product.title;
     price.textContent = `Price:${product.price}$`;
     description.textContent = product.description;
